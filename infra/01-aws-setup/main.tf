25e76d0c199b6feb36ca2dbd6c6604190f161d5a
Ravinder Dhamija <ravinderdhamija56@gmail.com>
2025-11-24 15:52:40 +0530

    Add docker image tagging

--- infra/01-aws-setup/main.tf
+++ infra/01-aws-setup/main.tf
@@ -28,17 +28,3 @@ module "vpc" {
     Terraform   = "true"
   }
 }
-
-resource "aws_ecr_repository" "app_repo" {
-  name                 = var.ecr_repo_name
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = false
-  }
-
-  tags = {
-    Environment = var.environment
-    Terraform   = "true"
-  }
-}
